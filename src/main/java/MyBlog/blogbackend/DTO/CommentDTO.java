ff804519cd37b51d11df6e27bad2e2b3ba7270ee
Martin Petrov <martuto31@gmail.com>
2023-12-28 19:37:28 +0200

    Added PostDTO

--- src/main/java/MyBlog/blogbackend/DTO/CommentDTO.java
+++ src/main/java/MyBlog/blogbackend/DTO/CommentDTO.java
@@ -1,5 +1,8 @@
 package MyBlog.blogbackend.DTO;
 
+import MyBlog.blogbackend.model.Post;
+import MyBlog.blogbackend.model.User;
+
 import java.time.LocalDateTime;
 import java.util.Set;
 
@@ -8,8 +11,8 @@ public class CommentDTO {
     private Long id;
     private String content;
     private LocalDateTime createdAt;
-    private Long postId;
-    private Long userId;
+    private Post post;
+    private User user;
     private Set<Long> tagIds;
 
     public CommentDTO() {
@@ -19,15 +22,15 @@ public class CommentDTO {
             Long id,
             String content,
             LocalDateTime createdAt,
-            Long postId,
-            Long userId,
+            Post post,
+            User user,
             Set<Long> tagIds) {
 
         this.id = id;
         this.content = content;
         this.createdAt = createdAt;
-        this.postId = postId;
-        this.userId = userId;
+        this.post = post;
+        this.user = user;
         this.tagIds = tagIds;
     }
 
@@ -43,13 +46,13 @@ public class CommentDTO {
 
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 
-    public Long getPostId() { return postId; }
+    public Post getPost() { return post; }
 
-    public void setPostId(Long postId) { this.postId = postId; }
+    public void setPost(Post post) { this.post = post; }
 
-    public Long getUserId() { return userId; }
+    public User getUser() { return user; }
 
-    public void setUserId(Long userId) { this.userId = userId; }
+    public void setUser(User user) { this.user = user; }
 
     public Set<Long> getTagIds() { return tagIds; }
 
