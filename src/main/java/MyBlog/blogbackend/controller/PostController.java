81261e44a67a7fd922251d1fe5b557c3e713f7eb
Martin Petrov <martuto31@gmail.com>
2024-01-03 01:06:10 +0200

    Added PostController Added UpdatePostDTO Refactored PostService

--- src/main/java/MyBlog/blogbackend/controller/PostController.java
+++ src/main/java/MyBlog/blogbackend/controller/PostController.java
@@ -1,4 +1,72 @@
 package MyBlog.blogbackend.controller;
 
+import java.util.List;
+
+import MyBlog.blogbackend.DTO.UpdatePostDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import MyBlog.blogbackend.service.post.PostService;
+
+import MyBlog.blogbackend.DTO.PostDTO;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO) {
+        PostDTO createdPost = postService.createPost(postDTO);
+
+        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<PostDTO> editPost(@PathVariable Long postId, @RequestBody UpdatePostDTO updatePostDTO) {
+        try {
+            PostDTO editedPost = postService.editPost(postId, updatePostDTO);
+
+            return new ResponseEntity<>(editedPost, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        try {
+            postService.deletePost(postId);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable Long postId) {
+        return postService.getPostById(postId)
+                .map(postDTO -> new ResponseEntity<>(postDTO, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostDTO>> getAllPosts() {
+        try {
+            List<PostDTO> posts = postService.getAllPosts();
+
+            return new ResponseEntity<>(posts, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
