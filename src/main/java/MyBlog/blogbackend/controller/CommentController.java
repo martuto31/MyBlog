b5fa9402efe4051f327411e046880dbe1226e600
Martin Petrov <martuto31@gmail.com>
2024-01-03 01:23:06 +0200

    Added CommentController Refactored CommentService

--- src/main/java/MyBlog/blogbackend/controller/CommentController.java
+++ src/main/java/MyBlog/blogbackend/controller/CommentController.java
@@ -1,4 +1,63 @@
 package MyBlog.blogbackend.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import MyBlog.blogbackend.service.comment.CommentService;
+
+import MyBlog.blogbackend.DTO.CommentDTO;
+
+@RestController
+@RequestMapping("/api/comments")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentDTO> createComment(@RequestBody CommentDTO commentDTO) {
+        CommentDTO createdComment = commentService.createComment(commentDTO);
+
+        return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<CommentDTO> editComment(@PathVariable Long commentId, @RequestBody String content) {
+        try {
+            CommentDTO editedComment = commentService.editComment(commentId, content);
+
+            return new ResponseEntity<>(editedComment, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
+        try {
+            commentService.deleteComment(commentId);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<List<CommentDTO>> getAllComments(@PathVariable Long postId) {
+        try {
+            List<CommentDTO> comments = commentService.getAllComments(postId);
+            return new ResponseEntity<>(comments, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
